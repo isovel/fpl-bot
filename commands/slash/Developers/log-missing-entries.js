ba66d9f97a1734622decc5a8ec986169c670a20a
MysticalEnnio <mystical.ennio@gmail.com>
2024-06-30 15:47:45 +0200

    Log missing entries and dont fix stuff

---
+++ commands/slash/Developers/log-missing-entries.js
@@ -0,0 +1,70 @@
+//Loop through all users. If they have a division or pending role but are not in the database add it to the message.
+//log everything in one discord message
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { log } = require('../../../functions');
+const { structure } = require('./view-applications');
+
+module.exports = {
+    structure: new SlashCommandBuilder()
+        .setName('log-missing-entries')
+        .setDescription(
+            'Log missing entries in the database for users with roles.'
+        ),
+    options: {
+        developers: true,
+    },
+    run: async (client, interaction) => {
+        const c_users = client.runtimeVariables.db.collection('users');
+        const roles = client.config.roles;
+        const divisionRoleA = roles.divisions['A'];
+        const divisionRoleB = roles.divisions['B'];
+        const pendingRole = roles['fpl-pending'];
+        const users = await interaction.guild.members.fetch();
+
+        let usersFound = 0;
+        let usersNotFound = 0;
+
+        let message = '';
+
+        for (const [_, member] of users) {
+            await setTimeout(async () => {
+                const user = await c_users.findOne({
+                    discordId: member.id,
+                });
+
+                if (!user) {
+                    usersNotFound++;
+                    log(`Users not found: ${usersNotFound}`, 'info');
+                    if (
+                        member.roles.cache.has(divisionRoleA) ||
+                        member.roles.cache.has(divisionRoleB)
+                    ) {
+                        log(
+                            `User ${member.displayName} is missing from the database but has the division role.`,
+                            'warn'
+                        );
+                        message += `User ${member.displayName} is missing from the database but has the division role. \n`;
+                    } else if (member.roles.cache.has(pendingRole)) {
+                        log(
+                            `User ${member.displayName} is missing from the database but has the pending role.`,
+                            'warn'
+                        );
+                        message += `User ${member.displayName} is missing from the database but has the pending role. \n`;
+                    }
+                }
+            }, 1000);
+        }
+
+        interaction.reply({
+            embeds: [
+                new EmbedBuilder()
+                    .setTitle('Missing Entries')
+                    .setDescription(
+                        `There are ${usersNotFound} missing entries in the database. \n\n${message}`
+                    )
+                    .setColor('Red'),
+            ],
+            ephemeral: client.config.development.ephemeral,
+        });
+    },
+};
