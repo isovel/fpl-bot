de4058266e6e212e449cdd9c82ef03541638e437
MysticalEnnio <mystical.ennio@gmail.com>
2024-07-04 01:15:24 +0200

    jsonTest2

--- api/test.js
+++ api/test.js
@@ -9,4 +9,8 @@ router.get('/jsonTest', (req, res) => {
     res.json(`This is a text test\nThis is another line`);
 });
 
+router.get('/jsonTest2', (req, res) => {
+    res.send(`This is a text test\nThis is another line`);
+});
+
 module.exports = router;
