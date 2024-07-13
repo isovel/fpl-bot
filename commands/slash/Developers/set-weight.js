20acdc5601054eecbf0fae1ba4f17c5d6967e648
Ennio Marke <mystical.ennio@gmail.com>
2024-07-13 21:16:54 +0200

    set-weight

---
+++ commands/slash/Developers/set-weight.js
@@ -0,0 +1,89 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const ExtendedClient = require('../../../class/ExtendedClient');
+const { log } = require('../../../functions');
+
+module.exports = {
+    structure: new SlashCommandBuilder()
+        .setName('set-weight')
+        .setDescription('Set your weight or the weight of a user.')
+        .addUserOption((option) =>
+            option
+                .setName('user')
+                .setDescription('The user to set the weight of.')
+                .setRequired(false)
+        )
+        .addIntegerOption((option) =>
+            option
+                .setName('weight')
+                .setDescription('The weight to set.')
+                .setRequired(true)
+        ),
+    run: async (client, interaction) => {
+        const user = interaction.member;
+        const weight = interaction.options.getInteger('weight');
+
+        //check if user has an application
+        const c_users = client.runtimeVariables.db.collection('users');
+        const userDoc = await c_users.findOne({
+            discordId: user.id,
+        });
+
+        if (!userDoc) {
+            return await interaction.reply({
+                embeds: [
+                    new EmbedBuilder()
+                        .setTitle('Error')
+                        .setDescription('User not found in the database.')
+                        .setColor('Red'),
+                ],
+                ephemeral: true,
+            });
+        }
+
+        log(`User weight: ${weight}`, 'debug');
+
+        await c_users.updateOne(
+            { discordId: user.id },
+            {
+                $set: {
+                    weight: weight,
+                },
+            }
+        );
+
+        const c_queues = client.runtimeVariables.db.collection('queues');
+
+        let queueData = await c_queues.findOne({
+            division: userDoc.division,
+        });
+
+        if (queueData.open && queueData.users.find((u) => u.id === user.id)) {
+            //update queue
+            await c_queues.updateOne(
+                {
+                    division: userDoc.division,
+                },
+                {
+                    $set: {
+                        'users.$[elem].weight': weight,
+                    },
+                },
+                {
+                    arrayFilters: [{ 'elem.id': user.id }],
+                }
+            );
+        }
+
+        await interaction.reply({
+            embeds: [
+                new EmbedBuilder()
+                    .setTitle('Weight Updated')
+                    .setDescription(
+                        `The weight has been updated to **${weight}**.`
+                    )
+                    .setColor('Green'),
+            ],
+            ephemeral: true,
+        });
+    },
+};
